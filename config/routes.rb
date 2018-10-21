0ef80a6b5488c7dffa8c3bd51fe3a0b7428b5e04
serpentz <absalomcherinet@gmail.com>
2018-10-21 15:52:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+
+
+
+	resources :songs 
+
+
+	
 end
---
+++ db/migrate/20181021181449_change_table_name_release_year.rb
@@ -0,0 +1,5 @@
+class ChangeTableNameReleaseYear < ActiveRecord::Migration
+  def change
+  		rename_column :songs, :released_year, :release_year
+  end
+end
